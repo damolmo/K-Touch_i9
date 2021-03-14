294eb2363d7060aa219d85e650468d9abaf628d3
David大伟 <davidmolinamorales1999@gmail.com>
2021-03-14 11:57:51 +0100

    Create build_i9.sh

---
+++ build_i9.sh
@@ -0,0 +1,43 @@
+#Install dependencies
+sudo apt update && sudo apt install git-core gnupg flex bison gperf zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip openjdk-8-jdk build-essential git fastboot adb
+
+#Install latest repo
+mkdir ~/bin
+PATH=~/bin:$PATH
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+
+#Install Android SDK Platform Tools
+wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+unzip platform-tools-latest-linux.zip -d ~
+cat >> ~/.profile<< EOF
+if [ -d "\$HOME/platform-tools" ] ; then
+    PATH="\$HOME/platform-tools:\$PATH"
+fi
+EOF
+
+
+#Set build cache to 50G
+export USE_CCACHE=1
+export CCACHE_DIR=~/.ccache
+source ~/.bashrc
+ccache -M 50G
+
+#Create twrp root folder
+mkdir -p twrp&&cd twrp
+
+#Download and sync TWRP manifest
+repo init -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0
+repo sync -j$(nproc --all)
+
+#Clone K-Touch i9 device tree and create device folder
+git clone -b TWRP-11-SAR https://github.com/daviiid99/K-Touch_i9.git device/ktouch/i9
+
+#Lunch command and build twrp
+. build/envsetup.sh
+lunch omni_i9-eng
+mka recoveryimage
+
+#Download and Flash TWRP 
+#out/target/product/i9/recovery.img #Result build will be inside this folder
+fastboot flash recovery out/target/product/i9/recovery.img
