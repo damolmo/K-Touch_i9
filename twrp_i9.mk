441a7c7c78df8ba72b497184af359072bd99c190
David <davidmolinamorales1999@gmail.com>
2021-08-03 23:34:18 +0200

    Update twrp_i9.mk

--- twrp_i9.mk
+++ twrp_i9.mk
@@ -9,6 +9,10 @@ $(call inherit-product, device/ktouch/i9/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/BoardConfigKernel.mk)
+$(call inherit-product, vendor/twrp/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/twrp/config/BoardConfigTWRP.mk)
+$(call inherit-product, vendor/twrp/config/packages.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9
