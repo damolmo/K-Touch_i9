cf0125cc234fea56ff1a46755b9eb23273da00dd
David <davidmolinamorales1999@gmail.com>
2021-08-04 19:24:17 +0200

    Update omni_i9.mk

--- omni_i9.mk
+++ omni_i9.mk
@@ -8,7 +8,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/ktouch/i9/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9
