92f770da404ae0709f0bb9864088f8eb5fc6cca5
David <davidmolinamorales1999@gmail.com>
2021-08-06 22:10:52 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -13,12 +13,3 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-service \
     bootctrl.$(PRODUCT_PLATFORM) \
     bootctrl.$(PRODUCT_PLATFORM).recovery \
-
-# tzdata
-PRODUCT_PACKAGES_ENG += \
-    tzdata_twrp
-
-# Apex libraries
-PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
-
