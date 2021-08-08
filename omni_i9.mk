d11772c38eb71fc866d880a1afa824a2c918e873
David <davidmolinamorales1999@gmail.com>
2021-08-08 10:25:13 +0200

    Update omni_i9.mk

--- omni_i9.mk
+++ omni_i9.mk
@@ -1,23 +1,15 @@
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Inherit from i9 device
-$(call inherit-product, device/ktouch/i9/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9
 PRODUCT_NAME := omni_i9
 PRODUCT_BRAND := Ktouch
-PRODUCT_MODEL := Ktouch
-PRODUCT_MANUFACTURER := ktouch
-PRODUCT_RELEASE_NAME := Ktouch Ktouch
+PRODUCT_MODEL := i9
+PRODUCT_MANUFACTURER := Ktouch
 
 # ADB Fix
 PRODUCT_PROPERTY_OVERRIDES += \
